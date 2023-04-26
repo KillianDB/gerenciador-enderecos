9b9fc00202445c42236f48090ed27b3caaa80c7c
KillianDB <killiandomingues57@gmail.com>
2023-04-26 10:38:37 -0300

    adicionando hooks para name e email e adicionando contador a pagina se registro de usuário

--- frontend/src/screens/UserRegisterScreen/index.jsx
+++ frontend/src/screens/UserRegisterScreen/index.jsx
@@ -1,27 +1,43 @@
 import './userRegisterScreen.css';
 import Header from '../../components/Header'
+import { useState } from 'react';
 
-function UserRegisterScreen(){
-    function handleSubmit(event){
-        event.preventDefault();
-      }
-    
-      return (
-        <>
-          <Header title = 'Cadastro de usuário'/>
-          <main>
-            <form onSubmit={handleSubmit}>
-              <label>Nome</label>
-              <input type="text" />
-    
-              <label>Email</label>
-              <input type="email" />
-    
-              <button type="submit">Cadastrar</button>
-            </form>
-          </main>
-        </>
-      );
+function UserRegisterScreen() {
+
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [contador, setContador] = useState(0);
+
+  function incrementar() {
+    setContador(contador + 1)
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    console.log(`Nome: ${name} | Email: ${email}`)
+  };
+
+  return (
+    <>
+      <Header title='Cadastro de usuário' />
+      <main>
+        <form onSubmit={handleSubmit}>
+          <label>Nome</label>
+          <input type="text" value={name} onChange={event => setName(event.target.value)} />
+
+          <label>Email</label>
+          <input type="email" value={email} onChange={event => setEmail(event.target.value)} />
+
+          <button type="submit">Cadastrar</button>
+        </form>
+        <section>
+          <p>Contador: {contador}</p>
+          <button onClick={incrementar}>Incrementa</button>
+        </section>
+      </main>
+    </>
+  );
 }
 
 export default UserRegisterScreen;
